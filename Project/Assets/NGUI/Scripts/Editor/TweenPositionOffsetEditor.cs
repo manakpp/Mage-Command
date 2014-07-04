cd5d6a3cb7dc3da80bd448cc19a120e7c6697dcf
Mana <mana.kpp@gmail.com>
2014-07-05 09:55:11 +1200

    NGUI and Shuriken magic

---
+++ Project/Assets/NGUI/Scripts/Editor/TweenPositionOffsetEditor.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using UnityEditor;
+
+[CustomEditor(typeof(TweenPositionOffset))]
+public class TweenPositionOffsetEditor : UITweenerEditor
+{
+	public override void OnInspectorGUI()
+	{
+		GUILayout.Space(6f);
+		NGUIEditorTools.SetLabelWidth(120f);
+
+		TweenPositionOffset tw = target as TweenPositionOffset;
+		GUI.changed = false;
+
+		Vector3 offset = EditorGUILayout.Vector3Field("From", tw.offset);
+
+		if (GUI.changed)
+		{
+			NGUIEditorTools.RegisterUndo("Tween Change", tw);
+			tw.Offset = offset;
+			NGUITools.SetDirty(tw);
+		}
+
+		DrawCommonProperties();
+	}
+}
---
+++ Project/Assets/NGUI/Scripts/Tweening/TweenPositionOffset.cs
@@ -0,0 +1,112 @@
+﻿using UnityEngine;
+
+/// <summary>
+/// Tween the object's position.
+/// </summary>
+
+[AddComponentMenu("NGUI/Tween/Tween Position Offset")]
+public class TweenPositionOffset : UITweener
+{
+	private Vector3 from;
+	private Vector3 to;
+	public Vector3 offset;
+
+	public Vector3 Offset
+	{
+		set 
+		{ 
+			offset = value;
+			to = from + offset;
+		}
+		get { return offset; }
+	}
+
+	[HideInInspector]
+	public bool worldSpace = false;
+
+	Transform mTrans;
+	UIRect mRect;
+
+	public Transform cachedTransform { get { if (mTrans == null) mTrans = transform; return mTrans; } }
+
+	[System.Obsolete("Use 'value' instead")]
+	public Vector3 position { get { return this.value; } set { this.value = value; } }
+
+
+	public void Initialise()
+	{
+		Start();
+	}
+
+
+	protected override void Start()
+	{
+		from = value;
+		to = from + offset;
+
+		base.Start();
+	}
+
+	/// <summary>
+	/// Tween's current value.
+	/// </summary>
+
+	public Vector3 value
+	{
+		get
+		{
+			return worldSpace ? cachedTransform.position : cachedTransform.localPosition;
+		}
+		set
+		{
+			if (mRect == null || !mRect.isAnchored || worldSpace)
+			{
+				if (worldSpace) cachedTransform.position = value;
+				else cachedTransform.localPosition = value;
+			}
+			else
+			{
+				value -= cachedTransform.localPosition;
+				NGUIMath.MoveRect(mRect, value.x, value.y);
+			}
+		}
+	}
+
+	void Awake () { mRect = GetComponent<UIRect>(); }
+
+	/// <summary>
+	/// Tween the value.
+	/// </summary>
+
+	protected override void OnUpdate (float factor, bool isFinished) { value = from * (1f - factor) + to * factor; }
+
+	/// <summary>
+	/// Start the tweening operation.
+	/// </summary>
+
+	static public TweenPosition Begin (GameObject go, float duration, Vector3 pos)
+	{
+		TweenPosition comp = UITweener.Begin<TweenPosition>(go, duration);
+		comp.from = comp.value;
+		comp.to = pos;
+
+		if (duration <= 0f)
+		{
+			comp.Sample(1f, true);
+			comp.enabled = false;
+		}
+		return comp;
+	}
+
+	[ContextMenu("Set 'From' to current value")]
+	public override void SetStartToCurrentValue () { from = value; }
+
+	[ContextMenu("Set 'To' to current value")]
+	public override void SetEndToCurrentValue () { to = value; }
+
+	[ContextMenu("Assume value of 'From'")]
+	void SetCurrentValueToStart () { value = from; }
+
+	[ContextMenu("Assume value of 'To'")]
+	void SetCurrentValueToEnd () { value = to; }
+}
