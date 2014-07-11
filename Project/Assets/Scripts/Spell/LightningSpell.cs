87aecf710ea1ea74acb220fbb20219931b5bb76a
Mana <mana.kpp@gmail.com>
2014-07-11 20:35:00 +1200

    Made a lightning strike and lightning spell.

---
+++ Project/Assets/Scripts/Spell/LightningSpell.cs
@@ -0,0 +1,46 @@
+﻿//  
+//  File Name   :   LightningSpell.cs
+//	Description	:	Lightning straight bolt of lightning in a swiped direction.
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class LightningSpell : Spell
+{
+
+	// Member Types
+
+
+	// Member Delegates & Events
+
+
+	// Member Properties
+
+
+	// Member Fields
+
+
+	// Member Methods
+
+
+	public override void SwipeCast(Mage _mage, Tile[] _targetTiles, float _swipeVelocity)
+	{
+		Vector3 startPosition = _targetTiles[0].transform.position;
+		Vector3 destination = _targetTiles[_targetTiles.Length - 1].transform.position;
+
+		var newProjectile = ObjectPool.Spawn(m_projectilePrefab.gameObject, startPosition);
+		if (newProjectile == null)
+			return;
+
+		var ball = newProjectile.GetComponent<Projectile>();
+		ball.Shoot(startPosition, destination);
+
+		if (ball.m_castSound != null)
+			AudioSource.PlayClipAtPoint(ball.m_castSound, Vector3.zero);
+	}
+};
---
+++ Project/Assets/Scripts/Spell/LightningStrikeSpell.cs
@@ -0,0 +1,45 @@
+﻿//  
+//  File Name   :   LightningStrikeSpell.cs
+//	Description	:	Lightning Strike will create a bolt of lightning directly down on a targetted tile.
+//
+
+
+// Namespaces
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class LightningStrikeSpell : Spell
+{
+
+	// Member Types
+
+
+	// Member Delegates & Events
+
+
+	// Member Properties
+
+
+	// Member Fields
+
+
+	// Member Methods
+
+
+	public override void TapCast(Mage _mage, Tile _targetTile)
+	{
+		Vector3 destination = _targetTile.transform.position;
+
+		var newProjectileObject = ObjectPool.Spawn(m_projectilePrefab.gameObject, destination);
+		if (newProjectileObject == null)
+			return;
+
+		var projectile = newProjectileObject.GetComponent<Projectile>();
+		projectile.Shoot(destination, destination);
+
+		if (projectile.m_castSound != null)
+			AudioSource.PlayClipAtPoint(projectile.m_castSound, Vector3.zero);
+	}
+};
